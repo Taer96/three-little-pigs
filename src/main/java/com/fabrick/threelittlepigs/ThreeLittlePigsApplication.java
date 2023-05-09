36eca9bdd51f85ba49155ff9e7a07248544eb64e
Giordano <giordano.piacentini@gmail.com>
2023-05-09 23:29:22 +0200

    first commit

---
+++ src/main/java/com/fabrick/threelittlepigs/ThreeLittlePigsApplication.java
@@ -0,0 +1,13 @@
+package com.fabrick.threelittlepigs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeLittlePigsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeLittlePigsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrick/threelittlepigs/ThreeLittlePigsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrick.threelittlepigs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeLittlePigsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
